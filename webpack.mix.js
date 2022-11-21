1276c4e634e9eaab3a42b983d3f8d8507696d55b
kibo13 <kimboris1310@gmail.com>
2022-11-22 01:10:27 +0600

    update(webpack): update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,6 +19,7 @@ mix .js('resources/js/admin/index.js', 'public/js/admin.js')
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendors/jquery.min.js')
 mix.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/vendors/bootstrap.min.js')
 mix.copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/vendors/jquery.dataTables.min.js')
+mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/vendors/bootstrap.min.css')
 
 if ( ! mix.inProduction() )
 {
