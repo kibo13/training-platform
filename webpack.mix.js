3166a536edbec05514fdf251d29874c0e104489d
kibo13 <kimboris1310@gmail.com>
2022-11-24 00:25:29 +0600

    config: add font-awesome.min.css package to webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,6 +21,7 @@ mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendors/jquery.min
 mix.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/vendors/bootstrap.min.js')
 mix.copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/vendors/jquery.dataTables.min.js')
 mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/vendors/bootstrap.min.css')
+mix.copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/css/vendors/font-awesome.min.css')
 
 if ( ! mix.inProduction() )
 {
