2e52baa66e2047dc0f52148d2c541dc2799e5f15
kibo13 <kimboris1310@gmail.com>
2022-11-24 02:13:12 +0600

    feat: add modal to confirm delete action

---
+++ resources/views/components/modals/delete.blade.php
@@ -0,0 +1,30 @@
+<div class="modal fade" id="bk-delete-modal">
+    <div class="modal-dialog modal-dialog-centered modal-sm">
+        <form class="modal-content" id="bk-delete-form" method="POST">
+            @csrf
+            @method('DELETE')
+
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    {{ __('_record.deleting') }}
+                </h5>
+                <button class="close" data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                {{ __('_dialog.delete') }}
+            </div>
+
+            <div class="modal-footer">
+                <button class="mr-0 btn btn-danger bk-min-w-50" type="submit">
+                    Да
+                </button>
+                <button class="btn btn-secondary bk-min-w-50" data-dismiss="modal">
+                    Нет
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
