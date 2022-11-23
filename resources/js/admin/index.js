1b988fccdd0bdd9e9c9f76037f006b7b95cff769
kibo13 <kimboris1310@gmail.com>
2022-11-24 02:17:06 +0600

    refactor(admin): update index files

--- resources/js/admin/index.js
+++ resources/js/admin/index.js
@@ -1,1 +1,12 @@
-console.log('admin working...')
+// components
+require('./components/sidebar')
+require('./components/datatable')
+require('./components/modal-delete')
+require('./components/alert')
+require('./components/button')
+require('./components/image')
+
+// custom
+require('./custom/validation')
+require('./custom/calculation')
+require('./custom/comparison')
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,6 +3,13 @@
 @section('content-head')
     <title>@yield('title-admin') | {{ config('app.name') }} </title>
 
+    <!-- vendors -->
+    <link rel="stylesheet" href="{{ asset('css/vendors/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/vendors/bootstrap.min.css') }}">
+    <script src="{{ asset('js/vendors/jquery.min.js') }}" defer></script>
+    <script src="{{ asset('js/vendors/bootstrap.min.js') }}" defer></script>
+    <script src="{{ asset('js/vendors/jquery.dataTables.min.js') }}" defer></script>
+
     <!-- custom -->
     <link rel="stylesheet" href="{{ mix('css/admin.css') }}">
     <script src="{{ mix('js/admin.js') }}" defer></script>
@@ -10,27 +17,14 @@
 
 @section('content-body')
     <div class="admin">
-        <h1>
-            @if(auth()->user()->hasRole('admin'))
-                Добро пожаловать, Администратор!
-            @elseif(auth()->user()->hasRole('teacher'))
-                Добро пожаловать, Учитель!
-            @else
-                Добро пожаловать, Ученик!
-            @endif
-        </h1>
-
-
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
+        @include('admin.partials.sidebar')
+        <div class="admin-wrapper">
+            <div class="admin-content">
+                @include('admin.partials.navbar')
+                @yield('content-admin')
+            </div>
         </div>
     </div>
+
+    @include('components.modals.delete')
 @endsection
